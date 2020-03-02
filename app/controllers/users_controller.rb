a93b96c6bbc71c003dd7907ad1cf9125f1b7d135
zdsmit <zdsmith001@gmail.com>
2020-03-02 12:31:55 -0800

    added edit function for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to @user
+  end
+
   private
 
   def user_params
