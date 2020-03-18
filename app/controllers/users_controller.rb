e508c7701998826d0b0b3f851a61e2f6672949ff
zdsmit <zdsmith001@gmail.com>
2020-03-18 11:45:48 -0700

    added user validations amd refactored signin to use email instead of name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    session[:user_id] = @user.id
-    redirect_to root_path
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to new_user_path
+    end
   end
 
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :transactions
   has_many :games, through: :transactions
-  scope :developer, -> { where(developer: true) }
+  scope :developers, -> { where(developer: true) }
+  validates :name, presence: true
+  validates :email, uniqueness: true
 
 end
